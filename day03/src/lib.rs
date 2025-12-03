a356d5b994f82d89375bdb0b1f768938b72c1f95
Matt Giltaji <matt@giltaji.com>
2025-12-03 15:14:06 -0800

    day 3 part 2

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -5,56 +5,76 @@ use std::{
 };
 
 /// Returns maximum joltage from a battery bank
-/// joltage is the combination of 2 digits in the passed-in string,
-///   with the left digit becoming the tens and right becoming the ones
+/// joltage is the combination of 12 digits in the passed-in string,
+///
 ///
 /// # Examples
 ///
 /// ```
-/// let joltage = day03::get_max_joltage(String::from("987654321111111"));
-/// assert_eq!(joltage, 98);
+/// let joltage = day03::get_max_joltage(String::from("98765432111111111111"));
+/// assert_eq!(joltage, 987654321111);
 /// ```
 /// ```
-/// let joltage = day03::get_max_joltage(String::from("123456789"));
-/// assert_eq!(joltage, 89);
+/// let joltage = day03::get_max_joltage(String::from("811111111111119"));
+/// assert_eq!(joltage, 811111111119);
 /// ```
 /// ```
 /// let joltage = day03::get_max_joltage(String::from("9239444444448"));
-/// assert_eq!(joltage, 99);
+/// assert_eq!(joltage, 939444444448);
 /// ```
-pub fn get_max_joltage(bank: String) -> u32 {
-    let mut first:u32 = 0;
-    let mut second:u32 = 0;
+pub fn get_max_joltage(bank: String) -> u64 {
+    println!("getting max from {bank}");
+    let mut highest:u32 = 0;
+    let mut jolt:u64 = 0;
     const RADIX:u32 = 10;
+    let mut stack: Vec<char> = Vec::new();
     let mut  mut_bank = bank;
-    //we won't be able to make a 2 digit number if we use the final digit as first,s o save it for later
-    let last = mut_bank.pop().expect("empty string");
 
-    for b in mut_bank.chars() {
-        let current = b.to_digit(RADIX).expect("non-digit char found");
-        if current > first {
-            first = current;
-        }
+    //pop off final 12 digits of string and put them in a stack
+    for _ in 1..12 {
+        let last = mut_bank.pop().expect("empty string");
+        stack.push(last);
     }
-    //add the final digit back
-    mut_bank.push(last);
-    let f = char::from_digit(first, RADIX).unwrap();
 
-    //only use digits after our selected first one
-    let (_, remaining) = mut_bank.split_once(f).unwrap();
-
-    for r in remaining.chars() {
-        let current = r.to_digit(RADIX).expect("non-digit char found");
-        if current > second {
-            second = current;
+    //split the remaining string at jolt spot
+    //pop top of the stack and append it to remaining string
+    //do it again until stack is empty
+    loop {
+        println!("getting highest digit from substring {mut_bank}");
+        //find largest digit in remaining string
+        for b in mut_bank.chars() {
+            let current = b.to_digit(RADIX).expect("non-digit char found");
+            if current > highest {
+                highest = current;
+            }
         }
+        jolt = jolt + highest as u64;
+        if stack.is_empty(){
+            break;
+        }
+
+        //shift jolt to next digit spot
+        jolt *= 10;
+
+
+        //add a stack digit to the end
+        let last = stack.pop().unwrap();
+        mut_bank.push(last);
+
+        //remove the front of the string, we used that already
+        let f = char::from_digit(highest, RADIX).unwrap();
+        let (_, remaining) = mut_bank.split_once(f).unwrap();
+
+
+        mut_bank = remaining.to_string();
+        println!("found {highest}, jolt: {jolt}, remaining search:{mut_bank}");
+        highest = 0;
     }
-    let result = first * 10 + second;
-    result
+    jolt
 }
 
-pub fn get_total_joltage(input:File) -> u32 {
-    let mut result:u32 = 0;
+pub fn get_total_joltage(input:File) -> u64 {
+    let mut result:u64 = 0;
     let buf = BufReader::new(input);
     for line in buf.lines() {
         let validated_line = line.expect("weird line");
@@ -75,7 +95,7 @@ mod tests {
         let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/test1.txt");
         let data = File::open(path).expect("test1.txt file missing");
         let result = get_total_joltage(data);
-        assert_eq!(result, 357);
+        assert_eq!(result, 3121910778619);
     }
 
     #[test]
@@ -83,6 +103,6 @@ mod tests {
         let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/input.txt");
         let data = File::open(path).expect("input.txt file missing");
         let result = get_total_joltage(data);
-        assert_eq!(result, 17321);
+        assert_eq!(result, 171989894144198);
     }
 }
